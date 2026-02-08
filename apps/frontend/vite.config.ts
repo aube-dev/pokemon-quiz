73598bc9e77dfcc6d482b72a34ce50fe1a5f8416
aube-dev <rufflain918@gmail.com>
2026-02-09 00:02:26 +0900

    feat: init project

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,10 @@
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    viteReact(),
+  ],
+});
