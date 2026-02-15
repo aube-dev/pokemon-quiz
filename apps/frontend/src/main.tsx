0d1b87e947602678dfd417647f782bdb9120c864
Gwangseo Go <rufflain918@gmail.com>
2026-02-15 17:01:46 +0900

    feat: add logo (incomplete)

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,14 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
-import { ThemeProvider } from "./components/theme-provider";
-import { ModeToggle } from "./components/ui/mode-toggle";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-      <ModeToggle className="fixed bottom-4 right-4" />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
--- apps/frontend/src/routes/login.tsx
+++ apps/frontend/src/routes/login.tsx
@@ -1,13 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { createFileRoute } from "@tanstack/react-router";
 import { LogIn } from "lucide-react";
 import { useState } from "react";
@@ -19,21 +12,19 @@ export const Route = createFileRoute("/login")({
 function RouteComponent() {
   const [info, setInfo] = useState<{
     roomNumber: string;
-    roomSeatSide?: "left" | "right";
-    roomSeatNumber: string;
     nickname: string;
     sn: string;
   }>({
     roomNumber: "",
-    roomSeatSide: undefined,
-    roomSeatNumber: "",
     nickname: "",
     sn: "",
   });
 
   return (
-    <div className="p-4 flex flex-col items-center gap-12">
-      <img src="/logo.png" className="size-48 rounded-full" />
+    <div className="flex flex-col items-center gap-12 p-4">
+      <div className="flex flex-col items-center gap-4">
+        <img src="/main-logo.jpg" className="size-60" />
+      </div>
       <div className="flex flex-col gap-4">
         <div className="flex flex-row items-center gap-4">
           <div className="flex flex-row items-center gap-1">
@@ -46,38 +37,6 @@ function RouteComponent() {
               }}
             />
             <Label className="shrink-0">생활관</Label>
-          </div>
-          <div className="flex flex-row items-center gap-1">
-            <Select
-              value={info.roomSeatSide}
-              onValueChange={(newValue) => {
-                setInfo((prev) => ({
-                  ...prev,
-                  roomSeatSide: newValue as "left" | "right",
-                }));
-              }}
-            >
-              <SelectTrigger>
-                <SelectValue placeholder="좌/우" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="left">좌</SelectItem>
-                <SelectItem value="right">우</SelectItem>
-              </SelectContent>
-            </Select>
-            <Input
-              type="number"
-              placeholder="생활관 자리"
-              className="text-sm"
-              value={info.roomSeatNumber}
-              onChange={(e) => {
-                setInfo((prev) => ({
-                  ...prev,
-                  roomSeatNumber: e.target.value,
-                }));
-              }}
-            />
-            <Label className="shrink-0">도</Label>
           </div>
         </div>
         <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2">
@@ -100,15 +59,7 @@ function RouteComponent() {
             }}
           />
         </div>
-        <Button
-          disabled={
-            !info.roomNumber ||
-            !info.roomSeatSide ||
-            !info.roomSeatNumber ||
-            !info.nickname ||
-            !info.sn
-          }
-        >
+        <Button disabled={!info.roomNumber || !info.nickname || !info.sn}>
           <LogIn /> 로그인
         </Button>
       </div>
